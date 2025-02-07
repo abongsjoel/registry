ae0d16b1466392155864668cf0e23ccdb86b8577
Joel Chi <abongsjoel@gmail.com>
2025-02-08 00:45:57 +0100

    Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import Root from "./pages/Root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import DetailsPage from "./pages/DetailsPage";
+
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <Root />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
