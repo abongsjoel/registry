12de028045ba54c6180808c73903f4e88fc17b08
Joel Chi <abongsjoel@gmail.com>
2025-02-10 15:02:03 +0100

    Add styling

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 
+import { VscSearch } from "react-icons/vsc";
 import { Form } from "react-router-dom";
 
 export default function SearchInput() {
@@ -21,7 +22,16 @@ export default function SearchInput() {
   //   );
   return (
     <Form action="/search">
-      <input name="term" />
+      <div className="relative">
+        <div className="absolute inset-y-0 flex items-center pl-3">
+          <VscSearch className="h-5 w-5 text-gray-500" />
+        </div>
+        <input
+          name="term"
+          className="pl-10 py-2 w-full border-0 shadow-none"
+          placeholder="Search packages"
+        />
+      </div>
     </Form>
   );
 }
