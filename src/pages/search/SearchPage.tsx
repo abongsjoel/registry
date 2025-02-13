2fdc2de1e10280211f96e256f6baf338223c4ca1
Joel Chi <abongsjoel@gmail.com>
2025-02-13 23:00:59 +0100

    Connect the page and loader

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,7 +1,8 @@
 import { useLoaderData } from "react-router-dom";
+import type { SearchLoaderResult } from "./searchLoader";
 
 export default function SearchPage() {
-  const data = useLoaderData();
+  const data = useLoaderData() as SearchLoaderResult;
   console.log({ data });
 
   return <div>SearchPage</div>;
