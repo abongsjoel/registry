05577b94a6b1b1aad6ef01e43cab98e9f8906a99
Joel Chi <abongsjoel@gmail.com>
2025-02-13 23:56:27 +0100

    Add details interface

---
+++ src/api/types/PackageDetails.ts
@@ -0,0 +1,14 @@
+export interface PackageDetials {
+  name: string;
+  description: string;
+  readme: string;
+  author: {
+    email: string;
+    name: string;
+  };
+  maintainers: {
+    email: string;
+    name: string;
+  }[];
+  license: string;
+}
